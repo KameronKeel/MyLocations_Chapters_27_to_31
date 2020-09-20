13b6a5792e036389b5ff9d76dc6540057d903ab8
Kameron Kkeel <Kkeel3@live.spcollege.edu>
2020-09-19 20:25:58 -0400

    Initial Commit

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Kameron Keel on 9/19/20.
+//  Copyright © 2020 Kameron Keel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Kameron Keel on 9/19/20.
+//  Copyright © 2020 Kameron Keel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
